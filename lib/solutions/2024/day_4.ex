45db5efb937249f62dc0804411054186f8e8414a
Eric Moon <moondawg2413@gmail.com>
2024-12-04 11:05:18 -0600

    Finish part 2

--- lib/solutions/2024/day_4.ex
+++ lib/solutions/2024/day_4.ex
@@ -28,20 +28,6 @@ defmodule Solutions.Year2024Day4 do
     end
   end
 
-  @doc """
-  iex> solve_part_2(#{inspect(@test_input)})
-  nil
-  """
-  def solve_part_2(_input) do
-    nil
-  end
-
-  defp parse_input(input) do
-    input
-    |> String.split("\n", trim: true)
-    |> Enum.map(&String.codepoints/1)
-  end
-
   defp get_word_count(puzzle, row, column) do
     translations = [
       {-1, -1},
@@ -82,13 +68,61 @@ defmodule Solutions.Year2024Day4 do
   end
 
   defp letter_found?(puzzle, row, column, letter) do
+    get_letter(puzzle, row, column) == letter
+  end
+
+  @doc """
+  iex> solve_part_2(#{inspect(@test_input)})
+  9
+  """
+  def solve_part_2(input) do
+    puzzle = parse_input(input)
+
+    {height, width} = get_bounds(puzzle)
+
+    x_translations = [
+      [{-1, -1}, {0, 0}, {1, 1}],
+      [{-1, 1}, {0, 0}, {1, -1}]
+    ]
+
+    for row <- 0..(height - 1), column <- 0..(width - 1), reduce: 0 do
+      acc ->
+        if get_letter(puzzle, row, column) == "A" do
+          x_translations
+          |> Enum.map(&get_x_word(puzzle, row, column, &1))
+          |> Enum.all?(fn word -> word in ["SAM", "MAS"] end)
+          |> if do
+            acc + 1
+          else
+            acc
+          end
+        else
+          acc
+        end
+    end
+  end
+
+  defp get_x_word(puzzle, row, column, translations) do
+    translations
+    |> Enum.map(fn {row_offset, column_offset} ->
+      get_letter(puzzle, row + row_offset, column + column_offset)
+    end)
+    |> Enum.join()
+  end
+
+  defp parse_input(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(&String.codepoints/1)
+  end
+
+  defp get_letter(puzzle, row, column) do
     if in_bounds?(puzzle, row, column) do
       puzzle
       |> Enum.at(row)
       |> Enum.at(column)
-      |> Kernel.==(letter)
     else
-      false
+      nil
     end
   end
 
--- test/day_test.exs
+++ test/day_test.exs
@@ -4,4 +4,5 @@ defmodule DayTest do
   doctest Solutions.Year2024Day1, import: true, tags: [:day_1]
   doctest Solutions.Year2024Day2, import: true, tags: [:day_2]
   doctest Solutions.Year2024Day3, import: true, tags: [:day_3]
+  doctest Solutions.Year2024Day4, import: true, tags: [:day_4]
 end
